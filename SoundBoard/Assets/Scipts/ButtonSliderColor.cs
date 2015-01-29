eccdb9449df9f4487c23919b68430ae517be7db0
Kevin Malmsten <outofspacegamedevelopment@gmail.com>
2015-01-29 17:50:01 -0600

    UI in Work

---
+++ SoundBoard/Assets/Scipts/ButtonSliderColor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonSliderColor : MonoBehaviour {
+
+	fadeInButton fadeScript;
+
+	void Start () 
+	{
+		GameObject fadeInScript = GameObject.FindGameObjectWithTag("FadeInButton");
+		fadeScript = fadeInScript.GetComponent<fadeInButton>();
+	}
+
+	void Update () 
+	{
+		if(fadeScript.fadeIn = true)
+		{
+			GetComponent<Image>().color = Color.green;
+		}
+		else
+		{
+			GetComponent<Image>().color = Color.red;
+		}
+	}
+}
---
+++ SoundBoard/Assets/Scipts/fadeInButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class fadeInButton : MonoBehaviour 
+{
+	public bool fadeIn = true;
+	public Slider fadeInSlider;
+
+	void Update()
+	{
+		if(fadeIn == true)
+		{
+			fadeInSlider.value = 1;
+		}
+		if(fadeIn == false)
+		{
+			fadeInSlider.value = 0;
+		}
+	}
+	public void Clicked()
+	{
+		Debug.Log("FadeIN");
+		fadeIn = !fadeIn;
+	}
+}
